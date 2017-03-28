a87f044550d6380f4ba82e363f4ed9c5a1a2e6ec
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2017-03-28 21:35:22 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,12 @@
+import PackageDescription
+
+let package = Package(
+    name: "NTP",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/mysql.git", majorVersion: 1)
+    ]
+)
