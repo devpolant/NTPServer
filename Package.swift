2094354d58c64dede7b507f2386cba6363898be7
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2017-03-28 21:44:54 +0300

    Compile sources. Add .xcodeproj to .gitignore

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "NTP",
+    name: "NTPServer",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,10 @@
-print("Hello, world!")
+import Foundation
+import Kitura
+import HeliumLogger
+
+HeliumLogger.use()
+
+let router = Router()
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
