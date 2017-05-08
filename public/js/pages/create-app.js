a3833e17732e34213d1b151cb7105ba8587d82a1
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2017-05-08 22:00:38 +0300

    Add Google Maps to web interface

--- public/js/pages/create-app.js
+++ public/js/pages/create-app.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
     $("#app-send-button").on("click", function () {
 
         var name = $("#inputAppName").val();
-        var location = $("#inputLocation").val();
+        var markerPosition = selectedLocation();
+        var location = markerPosition.lat() + ";" + markerPosition.lng();
         var socialGroup = $("#inputGroupName").val();
         $.ajax({
             url:"http://localhost:8090/dashboard/apps/create",
@@ -34,3 +35,38 @@ $(document).ready(function () {
 
     });
 });
+
+var markers = [];
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 50.0, lng: 36.2 }
+  });
+
+  map.addListener('click', function(e) {
+    placeMarkerAndPanTo(e.latLng, map);
+  });
+}
+
+function placeMarkerAndPanTo(latLng, map) {
+    clearMarkers();
+    var marker = new google.maps.Marker({
+        position: latLng,
+        animation: google.maps.Animation.DROP,
+        map: map
+    });
+    markers.push(marker);
+    map.panTo(latLng);
+}
+
+function clearMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
+}
+
+function selectedLocation() {
+    return markers[0].getPosition();
+}
